a2a1112ebc12fe35e6702d7fd92400dddaf82441
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2025-05-29 15:47:43 +0100

    format

--- openfisca_nouvelle_caledonie/variables/prelevements_obligatoires/impot_revenu/credits_impot.py
+++ openfisca_nouvelle_caledonie/variables/prelevements_obligatoires/impot_revenu/credits_impot.py
@@ -2,14 +2,13 @@
 
 import numpy as np
 
-
 from openfisca_core.model_api import *
 from openfisca_nouvelle_caledonie.entities import FoyerFiscal
-
 
 # Cadre 14 Autres réductions et crédits d'impôt
 
 ## Crédits et réductions d'impôts des entreprises
+
 
 class dons_organismes_aide_pme(Variable):
     unit = "currency"
@@ -76,6 +75,7 @@ class creche_entreprise(Variable):
 
 ## Autres crédits d'impôts
 
+
 class investissements_agrees_noumea_etc(Variable):
     unit = "currency"
     value_type = float
@@ -129,7 +129,6 @@ class credits_impot(Variable):
     definition_period = YEAR
 
     def formula(foyer_fiscal, period):
-
         impot_apres_reductions = foyer_fiscal("impot_apres_reductions", period)
         credits_resident = where(
             foyer_fiscal("resident", period),
@@ -137,49 +136,52 @@ class credits_impot(Variable):
                 foyer_fiscal("mecenat_entreprise", period)
                 + foyer_fiscal("depenses_exportation", period)
                 + foyer_fiscal("depenses_recherche_innovation", period)
-                ),
+            ),
             0,
-            )
+        )
 
-        solde_investissements_agrees = (
-            foyer_fiscal("solde_investissements_agrees_noumea_etc", period)
-            + foyer_fiscal("solde_investissements_agrees_autres", period)
-            )
+        solde_investissements_agrees = foyer_fiscal(
+            "solde_investissements_agrees_noumea_etc", period
+        ) + foyer_fiscal("solde_investissements_agrees_autres", period)
 
         credits_investissement = (
             foyer_fiscal("investissements_agrees_noumea_etc", period)
             + foyer_fiscal("investissements_agrees_autres", period)
             + foyer_fiscal("investissements_agrees_mixtes", period)
             + solde_investissements_agrees
-            )
+        )
 
         # Calcul des plafonds
         plaf_70 = where(
             credits_investissement > 0,
-            np.ceil(.70 * impot_apres_reductions),
+            np.ceil(0.70 * impot_apres_reductions),
             0,  # TODO: parameters
-            )
+        )
 
-        investissement_productif_industriel = foyer_fiscal("investissement_productif_industriel", period)
+        investissement_productif_industriel = foyer_fiscal(
+            "investissement_productif_industriel", period
+        )
         plaf_50 = where(
             investissement_productif_industriel > 0,
-            np.ceil(.50 * impot_apres_reductions),  # TODO: parameters
+            np.ceil(0.50 * impot_apres_reductions),  # TODO: parameters
             0,
-            )  # TOD0: manque case WE https://github.com/openfisca/openfisca-nouvelle_caledonie/issues/34
+        )  # TOD0: manque case WE https://github.com/openfisca/openfisca-nouvelle_caledonie/issues/34
 
         souscription_fcp = foyer_fiscal("souscription_fcp", period)
         plaf_60 = where(
             souscription_fcp > 0,
-            np.ceil(.60 * impot_apres_reductions),  # TODO: parameters
+            np.ceil(0.60 * impot_apres_reductions),  # TODO: parameters
             0,
-            )  # TOD0: manque case WW https://github.com/openfisca/openfisca-nouvelle_caledonie/issues/34
+        )  # TOD0: manque case WW https://github.com/openfisca/openfisca-nouvelle_caledonie/issues/34
 
-        mecenat_creche = foyer_fiscal("mecenat_entreprise", period) + foyer_fiscal("creche_entreprise", period)
+        mecenat_creche = foyer_fiscal("mecenat_entreprise", period) + foyer_fiscal(
+            "creche_entreprise", period
+        )
         plaf_100 = where(
             mecenat_creche > 0,
             impot_apres_reductions,
             0,
-            )
+        )
 
         # L'ensemble des crédits d'impôt ne peut dépasser le plus grand plafond
         plafond_credits = np.maximum.reduce([plaf_50, plaf_70, plaf_60, plaf_100])
@@ -192,47 +194,45 @@ class credits_impot(Variable):
         credit_solde_noumea_etc = where(
             solde_investissement_plafonnes > 0,
             np.ceil(
-                (foyer_fiscal("solde_investissements_agrees_noumea_etc", period) * retenue)
+                (
+                    foyer_fiscal("solde_investissements_agrees_noumea_etc", period)
+                    * retenue
+                )
                 / solde_investissements_agrees
-                ),
+            ),
             0,
-            )
+        )
         # retenue_wb
         credit_solde_autres = where(
             solde_investissement_plafonnes > 0,
             np.ceil(
                 (foyer_fiscal("solde_investissements_agrees_autres", period) * retenue)
                 / solde_investissements_agrees
-                ),
+            ),
             0,
-            )
+        )
 
         reliquat_plafond_credits = max_(
-            plafond_credits
-            - credit_solde_noumea_etc
-            - credit_solde_autres,
+            plafond_credits - credit_solde_noumea_etc - credit_solde_autres,
             0,
-            )
+        )
 
         # report
         report_solde_investissements_agrees_net = max_(
-            (
-                solde_investissements_agrees
-                - plaf_70
-                ),
+            (solde_investissements_agrees - plaf_70),
             0,
-            )
+        )
 
         # report_yo
-        report_investissements_agrees_noumea_= where(
+        report_investissements_agrees_noumea_ = where(
             solde_investissements_agrees > 0,
             (
                 foyer_fiscal("investissements_agrees_noumea_etc", period)
                 * report_solde_investissements_agrees_net
                 / solde_investissements_agrees
-                ),
+            ),
             0,
-            )
+        )
 
         # report wb
         report_investissements_agrees_noumea = where(
@@ -241,57 +241,61 @@ class credits_impot(Variable):
                 foyer_fiscal("solde_investissements_agrees_autres", period)
                 * report_solde_investissements_agrees_net
                 / solde_investissements_agrees
-                ),
+            ),
             0,
-            )
+        )
 
         # On reporte le reste sur les autres crédits d'impôt pour investissement     # On reporte le reste sur les autres crédits d'impôt pour investissement
-        credits_investissement_restants = (
-            np.ceil(
-                foyer_fiscal("investissements_agrees_noumea_etc", period)
-                + foyer_fiscal("investissements_agrees_autres", period)
-                )
-            + np.ceil(foyer_fiscal("investissements_agrees_mixtes", period))
-            )
+        credits_investissement_restants = np.ceil(
+            foyer_fiscal("investissements_agrees_noumea_etc", period)
+            + foyer_fiscal("investissements_agrees_autres", period)
+        ) + np.ceil(foyer_fiscal("investissements_agrees_mixtes", period))
         reliquet_credits_investissement_restants_plafonnes = min_(
             min_(
                 credits_investissement_restants,
                 plaf_70,
-                ),
+            ),
             reliquat_plafond_credits,
-            )
+        )
 
         # retenu_yo
-        credit_investissements_agrees_noumea_etc = np.ceil(where(
-            foyer_fiscal("investissements_agrees_noumea_etc", period) > 0,
-            (
-                foyer_fiscal("investissements_agrees_noumea_etc", period)
-                * reliquet_credits_investissement_restants_plafonnes
-                / credits_investissement_restants,
+        credit_investissements_agrees_noumea_etc = np.ceil(
+            where(
+                foyer_fiscal("investissements_agrees_noumea_etc", period) > 0,
+                (
+                    foyer_fiscal("investissements_agrees_noumea_etc", period)
+                    * reliquet_credits_investissement_restants_plafonnes
+                    / credits_investissement_restants,
                 ),
-            0,
-            ))
+                0,
+            )
+        )
 
         # retenu_yp
-        credit_investissements_agrees_autres = np.ceil(where(
-            foyer_fiscal("investissements_agrees_autres", period) > 0,
-            (
-                foyer_fiscal("investissements_agrees_autres", period)
-                * reliquet_credits_investissement_restants_plafonnes
-                / credits_investissement_restants
+        credit_investissements_agrees_autres = np.ceil(
+            where(
+                foyer_fiscal("investissements_agrees_autres", period) > 0,
+                (
+                    foyer_fiscal("investissements_agrees_autres", period)
+                    * reliquet_credits_investissement_restants_plafonnes
+                    / credits_investissement_restants
                 ),
-            0,
-            ))
+                0,
+            )
+        )
 
         # retenu_yz
-        credit_investissements_agrees_mixtes = np.ceil(where(
-            foyer_fiscal("investissements_agrees_mixtes", period) > 0,
-            (
-                foyer_fiscal("investissements_agrees_mixtes", period)
-                * reliquet_credits_investissement_restants_plafonnes
-                / credits_investissement_restants),
-            0,
-            ))
+        credit_investissements_agrees_mixtes = np.ceil(
+            where(
+                foyer_fiscal("investissements_agrees_mixtes", period) > 0,
+                (
+                    foyer_fiscal("investissements_agrees_mixtes", period)
+                    * reliquet_credits_investissement_restants_plafonnes
+                    / credits_investissement_restants
+                ),
+                0,
+            )
+        )
 
         # retenu_yz
         credit_investissements_agrees_mixtes = where(
@@ -300,45 +304,48 @@ class credits_impot(Variable):
             - credit_investissements_agrees_noumea_etc
             - credit_investissements_agrees_autres,
             credit_investissements_agrees_mixtes,
-            )
+        )
 
         # retenue yp
         credit_investissements_agrees_autres = where(
             (
                 (foyer_fiscal("investissements_agrees_autres", period) > 0)
                 & (foyer_fiscal("investissements_agrees_mixtes", period) == 0)
-                ),
+            ),
             (
                 reliquet_credits_investissement_restants_plafonnes
                 - credit_investissements_agrees_noumea_etc,
-                ),
+            ),
             credit_investissements_agrees_autres,
-            )
+        )
 
         reliquat_plafond_credits = max_(
             (
                 reliquat_plafond_credits
                 - credit_investissements_agrees_autres
                 - credit_investissements_agrees_mixtes
-                ),
+            ),
             0,
-            )
+        )
         report_investissements = where(
             credits_investissement_restants > plaf_70,
             credits_investissement_restants - plaf_70,
-            report_investissements_agrees_noumea
-            )
+            report_investissements_agrees_noumea,
+        )
         report_investissements_agrees_noumea = report_investissements
 
         # Amortissements excedentaires WE TODO; à vérifer et inclure
         credit_we = where(
             solde_investissement_plafonnes > 0,
             np.ceil(
-                (foyer_fiscal("solde_investissements_agrees_noumea_etc", period) * retenue)
+                (
+                    foyer_fiscal("solde_investissements_agrees_noumea_etc", period)
+                    * retenue
+                )
                 / solde_investissements_agrees
-                ),
+            ),
             0,
-            )
+        )
 
         # RETENUE_WE = min(we, plaf_we
         # RETENUE_W       = min(RETENUE_WE, plaf_credits)
@@ -353,36 +360,36 @@ class credits_impot(Variable):
         # YQ
         credit_investissement_productif_industriel = min_(
             min_(
-                .15 * foyer_fiscal("investissement_productif_industriel", period), # TODO: parameters
+                0.15
+                * foyer_fiscal(
+                    "investissement_productif_industriel", period
+                ),  # TODO: parameters
                 plaf_50,
-                ),
+            ),
             reliquat_plafond_credits,
-            )
+        )
 
         reliquat_plafond_credits = max_(
             reliquat_plafond_credits - reliquat_plafond_credits,
             0,
-            )
+        )
         # REPORT_YQ = retenue - plaf_yq if retenue > plaf_yq else
         # YV
         credit_souscription_fcp = min_(
             min_(
-                .15 * souscription_fcp,
+                0.15 * souscription_fcp,
                 plaf_50,
-                ),
+            ),
             reliquat_plafond_credits,
-            )
+        )
         reliquat_plafond_credits = max_(
-            reliquat_plafond_credits
-            - credit_souscription_fcp,
+            reliquat_plafond_credits - credit_souscription_fcp,
             0,
-            )
+        )
         report_yv = max_(
-            (
-                credit_souscription_fcp
-                - plaf_50),
+            (credit_souscription_fcp - plaf_50),
             0,
-            )
+        )
         credits_totaux = (
             credit_solde_noumea_etc
             + credit_solde_autres
@@ -392,54 +399,51 @@ class credits_impot(Variable):
             + credit_investissement_productif_industriel
             + credit_souscription_fcp
             # + credit_we
-            )
+        )
 
         # YG
         credit_creche_entreprise = min_(
-            .50 * min_(  # TODO: parameters
+            0.50
+            * min_(  # TODO: parameters
                 foyer_fiscal("creche_entreprise", period),
                 120_000_000,  # TODO: parameters
-                ),
+            ),
             60_000_000,  # TODO: parameters
-            )
+        )
         credit_creche_entreprise = min_(
             credit_creche_entreprise,
             impot_apres_reductions - credits_totaux,
-            )
+        )
 
         reliquat_plafond_credits = max_(
             reliquat_plafond_credits - credit_creche_entreprise,
             0,
-            )
+        )
 
         # YX
         credit_mecenat_entreprise = where(
             foyer_fiscal("resident", period),
             min_(
-                np.ceil(.80 * foyer_fiscal("mecenat_entreprise", period)),
+                np.ceil(0.80 * foyer_fiscal("mecenat_entreprise", period)),
                 impot_apres_reductions - credits_totaux,
-                ),
+            ),
             0,
-            )
+        )
 
         reliquat_plafond_credits = max_(
             reliquat_plafond_credits - credit_mecenat_entreprise,
             0,
-            )
+        )
 
         # WX
         credit_depenses_exportation = where(
             foyer_fiscal("resident", period),
             min_(
                 foyer_fiscal("depenses_exportation", period),
-                (
-                    impot_apres_reductions
-                    - credit_mecenat_entreprise
-                    - credits_totaux
-                    )
-                ),
+                (impot_apres_reductions - credit_mecenat_entreprise - credits_totaux),
+            ),
             0,
-            )
+        )
 
         # YS
         credit_depenses_recherche_innovation = where(
@@ -451,10 +455,10 @@ class credits_impot(Variable):
                     - credit_mecenat_entreprise
                     - credit_depenses_exportation
                     - credits_totaux
-                    ),
                 ),
+            ),
             0,
-            )
+        )
 
         return (
             credit_solde_noumea_etc
@@ -468,4 +472,4 @@ class credits_impot(Variable):
             + credit_mecenat_entreprise
             + credit_depenses_exportation
             + credit_depenses_recherche_innovation,
-            )
+        )
