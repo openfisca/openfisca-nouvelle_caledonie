da164c8e11a27d2a3c4e085ce77a32a4d947e91f
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2025-06-17 15:37:42 +0100

    Fix person leftovers

---
+++ openfisca_nouvelle_caledonie/variables/revenus/activite/salarie.py
@@ -0,0 +1,11 @@
+from openfisca_core.model_api import *
+from openfisca_nouvelle_caledonie.entities import Individu
+
+
+class salaire_de_base(Variable):
+    value_type = float
+    entity = Individu
+    label = "Salaire de base"
+    set_input = set_input_divide_by_period
+    definition_period = MONTH
+    unit = "currency"
