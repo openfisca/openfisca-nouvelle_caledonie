49edf9391bbe8ff1f3393a86ef7145889933fa4d
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2025-07-23 14:35:17 +0200

    WIP

---
+++ openfisca_nouvelle_caledonie/variables/prelevements_obligatoires/prelevements_sociaux/cotisations_sociales/cafat.py
@@ -0,0 +1,177 @@
+"""Cotisations sociales CAFATpour les salariés en Nouvelle-Calédonie."""
+
+from openfisca_core.model_api import *
+from openfisca_nouvelle_caledonie.entities import Individu
+from openfisca_nouvelle_caledonie.variables.prelevements_obligatoires.prelevements_sociaux.cotisations_sociales.helpers import (
+    apply_bareme,
+)
+from openfisca_nouvelle_caledonie.variables.prelevements_obligatoires.prelevements_sociaux.cotisations_sociales.salarie import (
+    TypesCategorieSalarie,
+)
+
+
+class accident_du_travail(Variable):
+    value_type = float
+    entity = Individu
+    label = "Cotisations employeur accident du travail et maladie professionelle"
+    definition_period = MONTH
+    set_input = set_input_divide_by_period
+
+    def formula_1991(
+        individu, period
+    ):  # TODO : rajouter formule pré-1991 : s'applique au salaire sous PSS uniquement
+        assiette = min_(
+            individu("salaire_de_base", period),
+            individu("plafond_cafat_autres_regimes", period),
+        )
+        taux_accident_du_travail = individu("taux_accident_du_travail", period)
+        categorie_salarie = individu("categorie_salarie", period)
+        assujetti = (categorie_salarie == TypesCategorieSalarie.prive_non_cadre) + (
+            categorie_salarie == TypesCategorieSalarie.prive_cadre
+        )
+        # TODO: ajouter contractuel du public salarié de moins d'un an ou à temps partiel
+        return assiette * taux_accident_du_travail * assujetti
+
+
+class chomage_employeur(Variable):
+    value_type = float
+    entity = Individu
+    label = "Cotisation chômage employeur"
+    definition_period = MONTH
+    set_input = set_input_divide_by_period
+
+    def formula(individu, period, parameters):
+        return apply_bareme(
+            individu,
+            period,
+            parameters,
+            cotisation_type="employeur",
+            bareme_name="chomage",
+            variable_name="chomage_employeur",
+        )
+
+
+class chomage_salarie(Variable):
+    value_type = float
+    entity = Individu
+    label = "Cotisation chômage salarié"
+    definition_period = MONTH
+    set_input = set_input_divide_by_period
+
+    def formula(individu, period, parameters):
+        return apply_bareme(
+            individu,
+            period,
+            parameters,
+            cotisation_type="salarie",
+            bareme_name="chomage",
+            variable_name="chomage_salarie",
+        )
+
+
+class fds(Variable):
+    value_type = float
+    entity = Individu
+    label = "Cotisation FDS employeur"
+    definition_period = MONTH
+    set_input = set_input_divide_by_period
+
+    def formula(individu, period, parameters):
+        return apply_bareme(
+            individu,
+            period,
+            parameters,
+            cotisation_type="employeur",
+            bareme_name="fds",
+            variable_name="fds",
+        )
+
+
+class fiaf(Variable):
+    value_type = float
+    entity = Individu
+    label = "Cotisation FIAF employeur"
+    definition_period = MONTH
+    set_input = set_input_divide_by_period
+
+    def formula(individu, period, parameters):
+        return apply_bareme(
+            individu,
+            period,
+            parameters,
+            cotisation_type="employeur",
+            bareme_name="fiaf",
+            variable_name="fiaf",
+        )
+
+
+class fsh(Variable):
+    value_type = float
+    entity = Individu
+    label = "Cotisation FSH employeur"
+    definition_period = MONTH
+    set_input = set_input_divide_by_period
+
+    def formula(individu, period, parameters):
+        return apply_bareme(
+            individu,
+            period,
+            parameters,
+            cotisation_type="employeur",
+            bareme_name="fsh",
+            variable_name="fsh",
+        )
+
+
+class prestations_familiales(Variable):
+    value_type = float
+    entity = Individu
+    label = "Cotisation prestations familiales (employeur seulement)"
+    definition_period = MONTH
+    set_input = set_input_divide_by_period
+
+    def formula(individu, period, parameters):
+        return apply_bareme(
+            individu,
+            period,
+            parameters,
+            cotisation_type="employeur",
+            bareme_name="prestations_familiales",
+            variable_name="prestations_familiales_employeur",
+        )
+
+
+class retraite_employeur(Variable):
+    value_type = float
+    entity = Individu
+    label = "Cotisation retraite employeur"
+    definition_period = MONTH
+    set_input = set_input_divide_by_period
+
+    def formula(individu, period, parameters):
+        return apply_bareme(
+            individu,
+            period,
+            parameters,
+            cotisation_type="employeur",
+            bareme_name="retraite",
+            variable_name="retraite_employeur",
+        )
+
+
+class retraite_salarie(Variable):
+    value_type = float
+    entity = Individu
+    label = "Cotisation retraite salarié"
+    definition_period = MONTH
+    set_input = set_input_divide_by_period
+
+    def formula(individu, period, parameters):
+        return apply_bareme(
+            individu,
+            period,
+            parameters,
+            cotisation_type="salarie",
+            bareme_name="retraite",
+            variable_name="retraite_salarie",
+        )
